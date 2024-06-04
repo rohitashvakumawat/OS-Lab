69170dde354dfbab7d6822fa39cd1cb13c1506a3
Rohitashva Kumawat <rohitashvakumawat2004@gmail.com>
2024-06-04 12:19:03 +0530

    Updated

--- add.sh
+++
@@ -1,5 +0,0 @@
-echo "Enter two numbers: "
-read n1 n2
-n3=`expr $n1+$n2`
-echo "n3 = $n3"
-echo "$[n3]"
--- commdline.sh
+++
@@ -1,5 +0,0 @@
-echo "Username: $1";
-echo "Age: $2";
-echo "Full Name: $3";
-
-#To run - sh userReg-positional-parameter.sh nikhil 20 'Rohit'
